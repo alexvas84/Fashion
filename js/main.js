65a9a68d56a9960dfa3742d891817dde4e304119
alexvas84 <alexvas84@gmail.com>
2020-05-15 09:16:58 +0300

    fix footer to bottom

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+
+	var mySwiper = new Swiper('.swiper-container', {
+
+		loop: true,
+
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+
+		autoplay: {
+			delay: 3000,
+		},
+
+	});
+
+
+	var mySwiper = new Swiper('.instagram__slider-container', {
+		containerModifierClass: 'instagram__slider-container',
+		slideClass: 'instagram__slider-slide',
+		wrapperClass: 'instagram__slider-wrapper',
+		slidesPerView: 6,
+		// slidesPerColumn: 1,
+		// spaceBetween: 20,
+		loop: true
+
+	})
+
+
+})
