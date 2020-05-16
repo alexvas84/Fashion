f833b5d5acf236f9862e844eb082746bd9a2c06a
alexvas84 <alexvas84@gmail.com>
2020-05-16 22:14:09 +0300

    make addaptive

--- js/main.js
+++ js/main.js
@@ -23,8 +23,40 @@ $(function () {
 		slidesPerView: 6,
 		// slidesPerColumn: 1,
 		// spaceBetween: 20,
-		loop: true
-	})
+		loop: true,
+		breakpoints: {
+			319: {
+				slidesPerView: 1,
+				spaceBetween: 21
+			},
+			630: {
+				slidesPerView: 2,
+				spaceBetween: 21
+			},
+			1230: {
+				slidesPerView: 3,
+				spaceBetween: 21
+			},
+			1250: {
+				slidesPerView: 4,
+				spaceBetween: 21
+			},
+			1550: {
+				slidesPerView: 5,
+				spaceBetween: 21
+			},
+			1850: {
+				slidesPerView: 6,
+				spaceBetween: 21
+			},
+		}
+	});
+
+	$('.header__menu-btn, .header__menu').on('click', function () {
+		$('.header__menu').toggleClass('active');
+		$('.header__menu-btn').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
 
 	/* 	$('.post-box .post__box-author, .post-box .post__box-item').hover(function (event) {
 			$('.post-box').toggleClass('active');
