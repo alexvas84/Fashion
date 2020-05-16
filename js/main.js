6476ecd400f54b2681e37d956cfca8d7da9ab623
alexvas84 <alexvas84@gmail.com>
2020-05-16 18:23:05 +0300

    add no shadow on hver

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ $(function () {
 
 	});
 
-
 	var mySwiper = new Swiper('.instagram__slider-container', {
 		containerModifierClass: 'instagram__slider-container',
 		slideClass: 'instagram__slider-slide',
@@ -25,8 +24,10 @@ $(function () {
 		// slidesPerColumn: 1,
 		// spaceBetween: 20,
 		loop: true
-
 	})
 
+	/* 	$('.post-box .post__box-author, .post-box .post__box-item').hover(function (event) {
+			$('.post-box').toggleClass('active');
+		}); */
 
 })
